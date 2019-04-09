708c2fd1cd8543f9e68575e877c8c366b722f8ec
Navjot Kukreja <navjotk@gmail.com>
2019-04-09 10:09:47 +0100

    Flake changes

--- examples/test_autodiff.py
+++ examples/test_autodiff.py
@@ -8,6 +8,6 @@ u = TimeFunction(name='u', grid=grid)
 u.data[:]
 u_i = u.indexed
 eq = Eq(u_i[t+1, x, y], 4*u_i[t, x, y] + 2*u_i[t, x-1, y] + 3*u_i[t, x+1, y])
-#eq2 = Eq(u.forward, u + 1)
+# eq2 = Eq(u.forward, u + 1)
 op = Operator(eq, adjoint=True)
 print("In the main program now")
