6ba35d0e731dc6168513973dec7bad48034f7d51
Tim Greaves <tim.greaves@imperial.ac.uk>
2019-05-10 22:22:58 +0100

    Last of the initial round of Dockerfile merges

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-find /app -type f -name '*.pyc' -delete
+find /home/devito -type f -name '*.pyc' -delete
 
 export PATH=/home/devito/.conda/envs/devito/bin:/usr/local/miniconda/condabin:$PATH
 export PYTHONPATH=$PYTHONPATH:/home/devito/.conda/envs/devito
