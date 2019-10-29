f6770b6a202dbe3f3de366bd0dd35048085ef00c
lucas cavalcante <thesupervisar@gmail.com>
2019-10-29 00:02:38 -0300

    wip

--- studies/utils.py
+++ studies/utils.py
@@ -4,29 +4,31 @@ import numpy as np
 
 def generate_data(shape, space_order=0, save=1):
     """
-    When creating a Function/TimeFunction with a ExternalAllocator, 
-    the shape of the initialized grid must consider the length 
-    of the borders imposed by space_order and the shape of the array 
-    given as input. 
+    Initialize a numpy array and a Grid object with compatible
+    shapes, considering the length of the borders imposed by 
+    space_order and the number of timesteps to be recorded.
     
-    Paramdeters
+    Parameters
     ----------
-    array : numpy.array
-        Array of data.
+    shape: tuple of int
+        Shape of the data grid, including implicit regions.
     space_order : int
         Space order of a Function/TimeFunction.
+    save : int
+        Number of stored timesteps of a TimeFunction.
 
     Returns
     -------
+    numpy_array
+        numpy_array with compatible shape.
     Grid
         Grid with specified shape. 
 
-    Grid 
-
     Notes
     -----
-    Tested for 2D cases.
+    Tested for 2D cases only.
     """
+
     grid_shape = tuple(x-2*y for x,y in zip(shape, (space_order,)*2))
     numpy_array = np.zeros(shape=(save,) + shape, dtype=np.float32)
 
