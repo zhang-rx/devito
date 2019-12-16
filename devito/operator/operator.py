d63ecf625189a8fbe91af0617e3ebea97fb1db6f
Fabio Luporini <f.luporini12@imperial.ac.uk>
2019-12-16 18:52:25 +0100

    Generalize Targets as Operators [pass 1]

--- devito/operator/operator.py
+++ devito/operator/operator.py
@@ -140,10 +140,12 @@ class Operator(Callable):
         # The Operator type for the given target
         cls = operator_selector(**kwargs)
 
+        # Lower to a JIT-compilable object
         with timed_region('op-compile') as r:
             op = cls._compile(expressions, **kwargs)
         op._profiler.py_timers.update(r.timings)
 
+        # Emit info about how long it took to perform the lowering
         op._emit_compile_profiling()
 
         return op
@@ -892,6 +894,9 @@ def parse_kwargs(**kwargs):
         kwargs['mode'] = 'noop'
     elif mode == 'noop':
         kwargs['mode'] = tuple(i for i in ['mpi', 'openmp'] if options[i]) or 'noop'
+    elif isinstance(mode, tuple):
+        if 'openmp' not in mode and options['openmp']:
+            mode += ('openmp',)
 
     # `dse`
     dse = kwargs.pop("dse", configuration['dse'])
@@ -905,5 +910,9 @@ def parse_kwargs(**kwargs):
             kwargs['dse'] = ','.join(dse)
         except:
             raise InvalidOperator("Illegal `dse=%s`" % str(dse))
+
+    # Attach `platform` too for convenience, basically so that we don't always have
+    # to query `configuration
+    kwargs['platform'] = configuration['platform']
 
     return kwargs
