28c23815982dfff9c3058aca9f2ecd9274b500b1
Fabio Luporini <f.luporini12@imperial.ac.uk>
2019-12-27 17:15:06 +0100

    passes: Initiate Cluster-level blocking

--- devito/ir/support/space.py
+++ devito/ir/support/space.py
@@ -370,7 +370,7 @@ class IntervalGroup(PartialOrderTuple):
             else:
                 intervals.append(i)
 
-        relations = []
+        relations = [dims]
         for r in self.relations:
             relation = []
             for i in r:
