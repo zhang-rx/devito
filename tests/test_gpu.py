e67e06a3de5da9a9d1ee8d098305d3b7d5ae7283
Rhodri Nelson <rhodri.nelson@gmail.com>
2020-01-10 14:59:11 +0000

    tests: Add iso-acoustic gpu test.

--- tests/test_gpu.py
+++ tests/test_gpu.py
@@ -66,3 +66,51 @@ class TestOffloading(object):
         op.apply(time_M=time_steps)
 
         assert np.all(np.array(u.data[0, :, :, :]) == time_steps)
+
+    @switchconfig(platform='nvidiaX')
+    def test_iso_ac(self):
+        from examples.seismic import Model, TimeAxis, RickerSource, Receiver
+        from devito import TimeFunction, solve, norm
+
+        shape = (101, 101)
+        spacing = (10., 10.)
+        origin = (0., 0.)
+
+        v = np.empty(shape, dtype=np.float32)
+        v[:, :51] = 1.5
+        v[:, 51:] = 2.5
+
+        model = Model(vp=v, origin=origin, shape=shape, spacing=spacing,
+                      space_order=2, nbl=10)
+
+        t0 = 0.
+        tn = 1000.
+        dt = model.critical_dt
+
+        time_range = TimeAxis(start=t0, stop=tn, step=dt)
+
+        f0 = 0.010
+        src = RickerSource(name='src', grid=model.grid, f0=f0,
+                           npoint=1, time_range=time_range)
+
+        src.coordinates.data[0, :] = np.array(model.domain_size) * .5
+        src.coordinates.data[0, -1] = 20.
+
+        rec = Receiver(name='rec', grid=model.grid, npoint=101, time_range=time_range)
+        rec.coordinates.data[:, 0] = np.linspace(0, model.domain_size[0], num=101)
+        rec.coordinates.data[:, 1] = 20.
+
+        u = TimeFunction(name="u", grid=model.grid, time_order=2, space_order=2)
+
+        pde = model.m * u.dt2 - u.laplace + model.damp * u.dt
+        stencil = Eq(u.forward, solve(pde, u.forward))
+
+        src_term = src.inject(field=u.forward, expr=src * dt**2 / model.m)
+
+        rec_term = rec.interpolate(expr=u.forward)
+
+        op = Operator([stencil] + src_term + rec_term, subs=model.spacing_map,
+                      dle=('advanced', {'openmp': True}))
+        op(time=time_range.num-1, dt=model.critical_dt)
+
+        assert np.isclose(norm(rec), 447.28362, atol=1e-3, rtol=0)
