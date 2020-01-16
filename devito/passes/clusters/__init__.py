b6ef2214e0a9f0cefc7ea8083f6df7b3b4475f11
Fabio Luporini <f.luporini12@imperial.ac.uk>
2020-01-16 15:54:47 +0100

    passes: Merge extractors into aliases

--- devito/passes/clusters/__init__.py
+++ devito/passes/clusters/__init__.py
@@ -2,7 +2,6 @@ from .utils import *  # noqa
 from .aliases import *  # noqa
 from .cse import *  # noqa
 from .factorization import *  # noqa
-from .extractors import *  # noqa
 from .blocking import *  # noqa
 from .interchange import *  # noqa
 from .misc import *  # noqa
