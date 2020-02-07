01a20b1ed9fb7db35c9850d6f0b446667b490673
Fabio Luporini <f.luporini12@imperial.ac.uk>
2020-02-07 09:47:26 +0100

    SQUASH WITH TESTS

--- tests/test_dse.py
+++ tests/test_dse.py
@@ -840,7 +840,7 @@ def test_tti_rewrite_aggressive_wmpi():
 
 @switchconfig(profiling='advanced')
 @pytest.mark.parametrize('space_order,expected', [
-    (8, 174), (16, 306)
+    (8, 177), (16, 313)
 ])
 def test_tti_rewrite_aggressive_opcounts(space_order, expected):
     operator = tti_operator(dse='aggressive', space_order=space_order)
